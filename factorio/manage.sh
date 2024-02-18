890bf0f9de38639cc9eed299c7f7a03506289eab
Cristian Moraru <cristian.moraru@live.com>
2024-02-18 23:25:22 +0100

    Added socket to factorio and updated start/stop script to new manage format.

---
+++ factorio/manage.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+  echo "Launch script with: --start | --stop | --save"
+  exit 1
+fi
+
+# shellcheck disable=SC1091
+source /etc/environment
+
+WORKING_DIR=/home/"$USER"/servers/factorio
+
+function start() {
+  exec "$WORKING_DIR"/install/bin/x64/factorio --start-server "$WORKING_DIR"/saves/tks.zip
+}
+
+function stop() {
+  save
+  sleep 10
+  echo "/quit" >./factorio.stdin
+}
+
+function save() {
+  echo "/save" >./factorio.stdin
+}
+
+#Read the argument values
+while [ $# -gt 0 ]; do
+  case "$1" in
+  --start)
+    start
+    shift
+    ;;
+  --stop)
+    stop
+    shift
+    ;;
+  --save)
+    save
+    shift
+    ;;
+  *)
+    shift
+    ;;
+  esac
+  shift
+done
--- factorio/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-WORKING_DIR=/home/"$USER"/servers/factorio
-
-exec "$WORKING_DIR"/install/bin/x64/factorio --start-server "$WORKING_DIR"/saves/tks.zip
