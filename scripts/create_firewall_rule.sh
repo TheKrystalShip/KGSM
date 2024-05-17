103731bdb99cf0546131be5f7451a65e4b512a0b
Cristian Moraru <cristian.moraru@live.com>
2024-05-18 01:35:45 +0200

    - Added script to create firewall rule file for each service, given a service name and port. Implemented only for UFW.

---
+++ scripts/create_firewall_rule.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Params
+if [ $# -eq 0 ]; then
+  echo ">>> ERROR: Service name not supplied. Run script like this: ./${0##*/} \"SERVICE\" \"PORT\""
+  exit 1
+fi
+if [ $# -eq 1 ]; then
+  echo ">>> ERROR: Service port not supplied. Run script like this: ./${0##*/} \"SERVICE\" \"PORT\""
+  exit 1
+fi
+
+SERVICE=$1
+PORT=$2
+
+# shellcheck disable=SC1091
+source /opt/scripts/service_vars.sh "$SERVICE"
+
+FIREWALL_FILE="ufw-$SERVICE_NAME"
+OUTPUT_FILE="$SERVICE_WORKING_DIR/service/$FIREWALL_FILE"
+
+function create_firewall_rule_file() {
+  cat >"$OUTPUT_FILE" <<-EOF
+[$SERVICE_NAME]
+title=$SERVICE_NAME
+description=$SERVICE_NAME
+ports=$PORT
+EOF
+}
+
+create_firewall_rule_file
