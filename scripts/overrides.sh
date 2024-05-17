dcbf6f37415a9b2b9c82d6b16546bceed75348e8
Cristian Moraru <cristian.moraru@live.com>
2024-05-18 01:31:49 +0200

    - Changed overrides.sh to always load service overrides if they are available, no more IS_STEAM_GAME check.

--- scripts/overrides.sh
+++ scripts/overrides.sh
@@ -12,12 +12,7 @@ SERVICE=$1
 source /opt/scripts/service_vars.sh "$SERVICE"
 
 # Import custom scripts if the game has any
-if [ "$IS_STEAM_GAME" == "0" ]; then
-  if ! test -f "$SERVICE_OVERRIDES_SCRIPT_FILE"; then
-    echo ">>> ERROR: Could not locate overrides.sh file for $SERVICE_NAME, exiting"
-    exit 1
-  fi
-
+if [ -f "$SERVICE_OVERRIDES_SCRIPT_FILE" ]; then
   # shellcheck disable=SC1090
   source "$SERVICE_OVERRIDES_SCRIPT_FILE"
 fi
