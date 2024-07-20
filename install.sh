c279411f80b83d8afac486702d0060ba3c5e1e81
Cristian Moraru <cristian.moraru@live.com>
2024-07-20 18:04:14 +0200

    Updated install script destination

--- install.sh
+++ install.sh
@@ -1,32 +1,24 @@
 #!/bin/bash
-# KGSM installer script
 
 set -e
 
+package="kgsm"
 head="main"
-version_url="https://raw.githubusercontent.com/TheKrystalShip/KGSM/$head/version.txt"
-repo_archive_url="https://github.com/TheKrystalShip/KGSM/archive/refs/heads/$head.tar.gz"
-tarball="kgsm-$head.tar.gz"
+destination="/usr/lib"
 
-required_packages=("grep" "jq" "wget" "unzip" "tar" "sed" "find")
-missing_packages=()
+repo_archive_url="https://raw.githubusercontent.com/TheKrystalShip/${package^^}/refs/heads/${head}.tar.gz"
+version_url="https://raw.githubusercontent.com/TheKrystalShip/${package^^}/${head}/version.txt"
 
-for p in "${required_packages[@]}"; do
-  if ! command -v "$p" &>/dev/null; then missing_packages+=("$p"); fi
-done
+version=$(wget -q -O - "$version_url")
+tarball="$package-$head-$version.tar.gz"
 
-if [[ "${#missing_packages[@]}" -ne 0 ]]; then
-  echo "ERROR: Missing required packages: ${missing_packages[@]}" >&2
-  echo "Please install missing packages before installing KGSM" >&2
-  exit 1
-fi
-
-latest_version=$(wget -q -O - "$version_url")
+cd "$destination"
+mkdir "$package"
+cd "$package"
 
 wget -O "$tarball" "$repo_archive_url" 2>/dev/null
 tar -xzf "$tarball"
 rm "$tarball"
-mv KGSM-$head kgsm
-cd kgsm || exit 1
-chmod +x kgsm.sh modules/*.sh
-echo "INFO: KGSM version $latest_version downloaded successfully." >&2
+mv ${package^^}-${head}/* .
+chmod +x ./*.sh ./modules/*.sh
+echo "KGSM $version successfully installed in $destination."
