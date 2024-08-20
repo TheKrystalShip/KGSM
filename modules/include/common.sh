bfdbaaf077facce64429dd88655472df6e132764
Cristian Moraru <cristian.moraru@live.com>
2024-08-20 15:43:30 +0200

    Added common functions to load instances, blueprints, modules and templates.

--- modules/include/common.sh
+++ modules/include/common.sh
@@ -33,3 +33,46 @@ export MODULES_INCLUDE_SOURCE_DIR=$MODULES_SOURCE_DIR/include
 
 # Directory where instances and their config is stored
 export INSTANCES_SOURCE_DIR=$KGSM_ROOT/instances
+
+function __find_or_fail() {
+  local file_name=$1
+  local source=${2:-$KGSM_ROOT}
+
+  local file_path
+  file_path=$(find "$source" -type f -name "$file_name" -print -quit)
+  [[ -z "$file_path" ]] && echo "${0##*/} ERROR: Could not find $file_name" >&2 && return 1
+
+  echo "$file_path"
+}
+
+export -f __find_or_fail
+
+function __load_blueprint() {
+  local blueprint=$1
+  [[ "$blueprint" != *.bp ]] && blueprint="${blueprint}.bp"
+  __find_or_fail "$blueprint" "$BLUEPRINTS_SOURCE_DIR"
+}
+
+export -f __load_blueprint
+
+function __load_module() {
+  local module=$1
+  [[ "$module" != *.sh ]] && module="${module}.sh"
+  __find_or_fail "$module" "$MODULES_SOURCE_DIR"
+}
+
+export -f __load_module
+
+function __load_instance() {
+  local instance=$1
+  [[ "$instance" != *.ini ]] && instance="${instance}.ini"
+  __find_or_fail "$instance" "$INSTANCES_SOURCE_DIR"
+}
+
+export -f __load_instance
+
+function __load_template() {
+  local template=$1
+  [[ "$template" != *.tp ]] && template="${template}.tp"
+  __find_or_fail "$template" "$TEMPLATES_SOURCE_DIR"
+}
