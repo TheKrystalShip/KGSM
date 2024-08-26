ae00572b44f7c2af9aff80ddec3079167252a435
Cristian Moraru <cristian.moraru@live.com>
2024-08-26 18:23:24 +0200

    Added default blueprint for Barotrauma

---
+++ overrides/barotrauma.overrides.sh
@@ -0,0 +1,125 @@
+#!/bin/bash
+
+################################################################################
+# Uncomment and use any of the following functions, they will be called from
+# other scripts at various stages of the install/update/backup/setup process.
+################################################################################
+#
+# Brief description of each:
+#
+# INPUT:
+# - void
+#
+# OUTPUT:
+# - echo "$version": Success
+# - exit 1: Error
+# func_get_latest_version       Should always return the latest available
+#                               version, or 1 in case there's any problem.
+#
+# INPUT:
+# - $1: Version
+# - $2: Destination directory, absolute path
+#
+# OUTPUT:
+# - exit 0: Success
+# - exit 1: Error
+# func_download                 In charge of downloading all the required files
+#                               for the service, extract any zips, move, copy,
+#                               rename, remove, etc. It should leave the $2
+#                               with a fully working setup that can be called
+#                               and executed as if it was a full install.
+#
+# INPUT:
+# - $1: Source directory, absolute path
+# - $2: Destination directory, absolute path
+#
+# OUTPUT:
+# - exit 0: Success
+# - exit 1: Error
+# func_deploy                   Will move everything from $1 into $2 and do any
+#                               cleanup that couldn't be done by func_download.
+#
+################################################################################
+#
+# Available global vars:
+#
+# INSTANCE_ID
+# INSTANCE_NAME
+# INSTANCE_FULL_NAME
+# INSTANCE_WORKING_DIR
+# INSTANCE_INSTALL_DIR
+# INSTANCE_SAVES_DIR
+# INSTANCE_TEMP_DIR
+# INSTANCE_BACKUPS_DIR
+# INSTANCE_LOGS_DIR
+# INSTANCE_INSTALL_DATETIME
+# INSTANCE_BLUEPRINT_FILE
+# INSTANCE_LEVEL_NAME
+# INSTANCE_PORT
+# INSTANCE_LAUNCH_BIN
+# INSTANCE_LAUNCH_ARGS
+# INSTANCE_LIFECYCLE_MANAGER
+# INSTANCE_MANAGE_FILE
+# INSTANCE_INSTALLED_VERSION
+#
+# (Optional) INSTANCE_STOP_COMMAND
+# (Optional) INSTANCE_SAVE_COMMAND
+# (Optional) INSTANCE_PID_FILE
+# (Optional) INSTANCE_OVERRIDES_FILE
+# (Optional) INSTANCE_UFW_FILE
+# (Optional) INSTANCE_SYSTEMD_SERVICE_FILE
+# (Optional) INSTANCE_SYSTEMD_SOCKET_FILE
+################################################################################
+
+
+
+# INPUT:
+# - void
+#
+# OUTPUT:
+# - void: Success (echo "$new_version")
+# - 1: Error
+# function func_get_latest_version() {
+#   echo ""
+# }
+
+# INPUT:
+# - $1: Version
+# - $2: Destination directory, absolute path
+#
+# OUTPUT:
+# - 0: Success
+# - 1: Error
+# function func_download() {
+#   local version=$1
+#   local dest=$2
+#
+#   return 0
+# }
+
+# INPUT:
+# - $1: Source directory, absolute path
+# - $2: Destination directory, absolute path
+#
+# OUTPUT:
+# - 0: Success
+# - 1: Error
+function func_deploy() {
+  local source=$1
+  local dest=$2
+
+  if ! cp -r "$source"/* "$dest"; then
+    echo "${0##*/} ERROR: Failed to copy $source into $dest" >&2 && return 1
+  fi
+
+  if ! rm -rf "${source:?}/*"; then
+    echo "${0##*/} ERROR: Failed to clear $source" >&2 && return 1
+  fi
+
+  # https://barotraumagame.com/wiki/Hosting_a_Dedicated_Server#Linux_Dedicated_Server_Hosting
+  if ! mkdir -p "${HOME}/.local/share/Daedalic Entertainment GmbH/Barotrauma"; then
+    echo "${0##*/} ERROR: Failed to create required directory" >&2 && return 1
+  fi
+
+  return 0
+}
