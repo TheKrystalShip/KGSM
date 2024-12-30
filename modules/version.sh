3011fa52fd20e42c475405165e97f649bc1ffd00
Cristian Moraru <cristian.moraru@live.com>
2024-12-30 13:25:56 +0100

    Formatting for version.sh module

--- modules/version.sh
+++ modules/version.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Disabling SC2086 globally:
+# Exit code variables are guaranteed to be numeric and safe for unquoted use.
+# shellcheck disable=SC2086
+
 function usage() {
   echo "Used to fetch various version informations.
 
@@ -39,8 +43,8 @@ if [[ $@ =~ "--debug" ]]; then
   for a; do
     shift
     case $a in
-    --debug) continue ;;
-    *) set -- "$@" "$a" ;;
+      --debug) continue ;;
+      *) set -- "$@" "$a" ;;
     esac
   done
 fi
@@ -50,17 +54,17 @@ fi
 # Read the argument values
 while [[ "$#" -gt 0 ]]; do
   case $1 in
-  -h | --help)
-    usage && exit 0
-    ;;
-  -i | --instance)
-    shift
-    [[ -z "$1" ]] && echo "${0##*/} ERROR: Missing argument <instance>" >&2 && exit 1
-    instance=$1
-    ;;
-  *)
-    break
-    ;;
+    -h | --help)
+      usage && exit 0
+      ;;
+    -i | --instance)
+      shift
+      [[ -z "$1" ]] && echo "${0##*/} ERROR: Missing argument <instance>" >&2 && exit 1
+      instance=$1
+      ;;
+    *)
+      break
+      ;;
   esac
   shift
 done
@@ -88,21 +92,21 @@ module_overrides=$(__load_module overrides.sh)
 
 instance_config_file=$(__load_instance "$instance")
 # shellcheck disable=SC1090
-source "$instance_config_file" || exit "$EC_FAILED_SOURCE"
+source "$instance_config_file" || exit $EC_FAILED_SOURCE
 
 # https://github.com/ValveSoftware/steam-for-linux/issues/10975
 function func_get_latest_version() {
 
-  app_id="$(grep "BP_APP_ID=" <"$INSTANCE_BLUEPRINT_FILE" | cut -d '=' -f2 | tr -d '"')"
+  app_id="$(grep "BP_APP_ID=" < "$INSTANCE_BLUEPRINT_FILE" | cut -d '=' -f2 | tr -d '"')"
   {
-    [[ -z "$app_id" ]] || [[ "$app_id" -eq 0 ]];
-  } && __print_error "APP_ID is expected but it's not set" && return "$EC_MALFORMED_INSTANCE"
+    [[ -z "$app_id" ]] || [[ "$app_id" -eq 0 ]]
+  } && __print_error "APP_ID is expected but it's not set" && return $EC_MALFORMED_INSTANCE
 
   username=anonymous
-  auth_level="$(grep "BP_STEAM_AUTH_LEVEL=" <"$INSTANCE_BLUEPRINT_FILE" | cut -d '=' -f2 | tr -d '"')"
+  auth_level="$(grep "BP_STEAM_AUTH_LEVEL=" < "$INSTANCE_BLUEPRINT_FILE" | cut -d '=' -f2 | tr -d '"')"
   if [[ $auth_level -ne 0 ]]; then
-    [[ -z "$STEAM_USERNAME" ]] && __print_error "STEAM_USERNAME is expected but it's not set" && return "$EC_MISSING_ARG"
-    [[ -z "$STEAM_PASSWORD" ]] && __print_error "STEAM_PASSWORD is expected but it's not set" && return "$EC_MISSING_ARG"
+    [[ -z "$STEAM_USERNAME" ]] && __print_error "STEAM_USERNAME is expected but it's not set" && return $EC_MISSING_ARG
+    [[ -z "$STEAM_PASSWORD" ]] && __print_error "STEAM_PASSWORD is expected but it's not set" && return $EC_MISSING_ARG
 
     username="$STEAM_USERNAME $STEAM_PASSWORD"
   fi
@@ -116,18 +120,22 @@ function func_get_latest_version() {
     --color=NEVER \
     -Po '"branches"\s*{\s*"public"\s*{\s*"buildid"\s*"\K(\d*)')
 
-  [[ -z "$latest_version" ]] && return "$EC_GENERAL"
+  if [[ -z "$latest_version" ]]; then
+    __print_error "Failed to retrieve latest version, got empty response from SteamCMD"
+    return "$EC_GENERAL"
+  fi
+
   echo "$latest_version"
 }
 
 function _compare() {
-  [[ -z "$INSTANCE_INSTALLED_VERSION" ]] && __print_error "$instance is missing INSTANCE_INSTALLED_VERSION varible" && return "$EC_MALFORMED_INSTANCE"
+  [[ -z "$INSTANCE_INSTALLED_VERSION" ]] && __print_error "$instance is missing INSTANCE_INSTALLED_VERSION varible" && return $EC_MALFORMED_INSTANCE
 
   local latest_version
   latest_version=$(func_get_latest_version)
 
-  [[ -z "$latest_version" ]] && return "$EC_GENERAL"
-  [[ "$latest_version" == "$INSTANCE_INSTALLED_VERSION" ]] && return "$EC_GENERAL"
+  [[ -z "$latest_version" ]] && return $EC_GENERAL
+  [[ "$latest_version" == "$INSTANCE_INSTALLED_VERSION" ]] && return $EC_GENERAL
 
   echo "$latest_version"
 }
@@ -135,19 +143,19 @@ function _compare() {
 function _save_version() {
   local version=$1
 
-  if grep -q "INSTANCE_INSTALLED_VERSION=" <"$instance_config_file"; then
-    if ! sed -i "/INSTANCE_INSTALLED_VERSION=*/c\INSTANCE_INSTALLED_VERSION=$version" "$instance_config_file" >/dev/null; then
-      return "$EC_FAILED_SED"
+  if grep -q "INSTANCE_INSTALLED_VERSION=" < "$instance_config_file"; then
+    if ! sed -i "/INSTANCE_INSTALLED_VERSION=*/c\INSTANCE_INSTALLED_VERSION=$version" "$instance_config_file" > /dev/null; then
+      return $EC_FAILED_SED
     fi
   else
     {
       echo ""
       echo "# Installed version"
       echo "INSTANCE_INSTALLED_VERSION=$version"
-    } >>"$instance_config_file"
+    } >> "$instance_config_file"
   fi
 
-  echo "$version" >"${INSTANCE_WORKING_DIR}/${INSTANCE_FULL_NAME}.version"
+  echo "$version" > "${INSTANCE_WORKING_DIR}/${INSTANCE_FULL_NAME}.version"
 
   __emit_instance_version_updated "${instance%.ini}" "$INSTANCE_INSTALLED_VERSION" "$version"
 
@@ -160,23 +168,26 @@ source "$module_overrides" "$instance"
 # Read the argument values
 while [[ "$#" -gt 0 ]]; do
   case $1 in
-  --compare)
-    _compare; exit $?
-    ;;
-  --installed)
-    echo "$INSTANCE_INSTALLED_VERSION" && exit 0
-    ;;
-  --latest)
-    func_get_latest_version; exit $?
-    ;;
-  --save)
-    shift
-    [[ -z "$1" ]] && __print_error "Missing argument <version>" && exit "$EC_MISSING_ARG"
-    _save_version "$1"; exit $?
-    ;;
-  *)
-    __print_error "Invalid argument $1" && exit "$EC_INVALID_ARG"
-    ;;
+    --compare)
+      _compare
+      exit $?
+      ;;
+    --installed)
+      echo "$INSTANCE_INSTALLED_VERSION" && exit 0
+      ;;
+    --latest)
+      func_get_latest_version
+      exit $?
+      ;;
+    --save)
+      shift
+      [[ -z "$1" ]] && __print_error "Missing argument <version>" && exit $EC_MISSING_ARG
+      _save_version "$1"
+      exit $?
+      ;;
+    *)
+      __print_error "Invalid argument $1" && exit $EC_INVALID_ARG
+      ;;
   esac
   shift
 done
